2ba05185726574f866ccd5460b8e7c0870040240
John Colarusso <8105718+johncolarusso@users.noreply.github.com>
2026-05-05 19:55:42 -0700

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        PriorityQueue<Pair> pq = new PriorityQueue(
+            // Comparator.comparingInt(p -> ((Pair) p).count()).reversed()
+            (a, b) -> ((Pair)a).count() - ((Pair)b).count()
+        );
+
+        for (int n : nums) {
+            counts.put(n, counts.getOrDefault(n, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> e : counts.entrySet()) {
+            pq.add(new Pair(e.getKey(), e.getValue()));
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll().item();
+        }
+
+        return result;
+    }
+    record Pair(int item, int count){};
+    // class Pair {
+    //     int item;
+    //     int count;
+
+    //     public Pair(int item, int count) {
+    //         this.item = item;
+    //         this.
+    //     }
+    // }
+}
